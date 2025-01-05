51a48a7fb6df5bd852a9d2d1268b6fafcdf751bb
MFaad1 <faadsardar123@gmail.com>
2025-01-05 21:19:43 +0500

    New commit and changes added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='flex justify-center items-center text-16 sm:text-2xl font-semibold mt-2'>About Us</div>
+    <div className='flex justify-center items-center text-16 sm:text-2xl font-semibold mt-2'>\
+    
+    About Us
+    </div>
   )
 };
 
