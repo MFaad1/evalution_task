f91ebce03c517041215bbf430f774d68686cc77a
MFaad1 <faadsardar123@gmail.com>
2025-02-16 21:26:39 +0500

    fixes

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Wishlist = () => {
   return (
     <div className='flex justify-center items-center text-16 sm:text-2xl font-semibold mt-2'>Wishlist</div>
-  )
-};
 
-export default Wishlist;
+
+  )
+
+}
+
+
+
+export default Wishlist
